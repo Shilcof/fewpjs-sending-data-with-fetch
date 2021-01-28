7a54dbb139b0bfd92fbc6a940696502549802f71
Shilcof <robert.shilcof@gmail.com>
2021-01-28 11:49:46 +0000

    commit tree ae497c5958f47dd1cd70f2adf959c1a5cfb498ce

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(name, email) {
+    const configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    };
+    return fetch("http://localhost:3000/users", configObject)
+        .then(function(responce) { 
+            return responce.json();    
+        })
+        .then(function(object) {
+            appendToBody(object["id"]);
+        })
+        .catch(function(error) {
+            appendToBody(error.message);
+        });
+}
+
+function appendToBody(message) {
+    const messageDiv = document.createElement("div");
+    messageDiv.textContent = message;
+    document.querySelector('body').append(messageDiv);
+}
